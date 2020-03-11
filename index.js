4a95823128a8919feec63d6d2ef64a033eb24d0e
gjcasanovao <casanova.gabriel@gmail.com>
2020-03-11 12:56:33 -0300

    Commit inicial con servicio simple

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+app.listen(3000, () => {
+
+
+app.get('/', function (req, res) {
+  res.send('Saludos desde express');
+});
+
+app.post('/hola', function (req, res) {
+  res.send('[POST]Saludos desde express');
+});
+app.get('/hola', function (req, res) {
+  res.send('[GET]Saludos desde express');
+});
+
+ console.log("El servidor está inicializado en el puerto 3000");
+});
